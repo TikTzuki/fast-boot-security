1b8631bde98e63a4b2abee2f60bb843146d50b4e
longtpt <longtpt@minerva.vn>
2022-04-13 23:58:24 +0700

    update package

---
+++ src/fast_boot/security/access/intercept/integration_filter.py
@@ -0,0 +1,6 @@
+from src.fast_boot.schemas import Filter
+
+
+class IntegrationFilter(Filter):
+    async def do_filter(self, request, response, filter_chain) -> None:
+        pass
--- src/security/access/intercept/integration_filter.py
+++
@@ -1,6 +0,0 @@
-from src.schemas import Filter
-
-
-class IntegrationFilter(Filter):
-    async def do_filter(self, request, response, filter_chain) -> None:
-        pass
